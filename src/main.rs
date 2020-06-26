8c635800012e10f24fbe1de058d73814a5077226
John Dowson <dowson.john.pony@gmail.com>
2020-06-26 05:40:32 +0300

    Generalized waveform function interface

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,9 @@ use waves::*;
 pub mod trakparse;
 pub mod types;
 use types::*;
+const PITCH: Hz = 440.; //E 329.63;
+const SAMPLERATE: f64 = 48000.;
+const BPM: f64 = 136.;
 
 fn attack(i: usize, _duration: usize) -> f64 {
     let attack_length = 1000;
@@ -40,19 +43,20 @@ fn lerp(from: f64, to: f64, scalar: f64) -> f64 {
     from + (to - from) * scalar
 }
 
-fn freq(p: Note, waveform: fn(f64) -> f64) -> Wave {
+fn freq(p: Note, waveform: fn(f64, f64) -> f64) -> Wave {
     match p {
         Note::Silence(duration) => return vec![0f64; (duration * SAMPLERATE) as usize],
         Note::Note(hz, duration) => {
             let total_samples = (duration * SAMPLERATE) as usize;
             let volume = 0.5;
-            let freq = (2.* PI * hz) / SAMPLERATE; //((hz * 2.0) as f64 * PI) / SAMPLERATE;
+            //let freq = (2. * PI * hz) / SAMPLERATE; //((hz * 2.0) as f64 * PI) / SAMPLERATE;
             return linspace::<f64>(0., duration * SAMPLERATE, total_samples)
                 .enumerate()
                 .map(|x| {
-                    let mut step = waveform(x.1 * freq);
-                    step = (attack(x.0, total_samples) * step) * decay(x.0, total_samples);
-                    step * volume
+                    let (step, t) = x;
+                    let mut sample = waveform(hz, t);
+                    sample = (attack(step, total_samples) * sample) * decay(step, total_samples);
+                    sample * volume
                 })
                 .map(|x| x as f64)
                 .collect();
@@ -67,14 +71,11 @@ fn write(p: &str, w: Wave) {
     LittleEndian::write_f64_into(&w, &mut b);
     f.write_all(&b).expect("Can't write to specified file");
 }
-const PITCH: Hz = 440.;//E 329.63;
-const SAMPLERATE: f64 = 48000.;
-const BPM: f64 = 136.;
 
 fn main() {
     let t1 = Instant::now();
     let wave: &[Interval];
-    wave = &[
+    /*wave = &[
         Interval::Note(7., 0.5),
         Interval::Note(7., 0.5),
         Interval::Note(7., 0.25),
@@ -252,18 +253,20 @@ fn main() {
         Interval::Note(10., 0.5),
         Interval::Note(10., 0.5),
         Interval::Hold(4.),
-    ];
+    ];*/
+    wave = &[Interval::Note(0., 10.)];
     let wave = wave.iter().map(|x| x.to_note(BPM, PITCH));
     //use rand::prelude::*;
     write(
         &"out.bin",
         wave.map(|i| {
-            freq(i, {
+            freq(
+                i,
                 //|x| (phat_sine(x / 1.5) + squeaky(x * 1.5) / 2. + square(x / 2.) + noize(x)) / 4.
                 //|x| (phat_sine(x/2.)/2. + squeaky(x/1.5)/2. + square(x)/2.)
                 //|x| *[sine(-x), sawtooth(x)].choose(&mut thread_rng()).unwrap()
-                |x| sine(x)
-            })
+                |f, t| sine(sine(-f, t), t),
+            )
         })
         .flatten()
         .collect(),
--- src/waves.rs
+++ src/waves.rs
@@ -1,38 +1,41 @@
 use std::f64::consts::PI;
-#[allow(dead_code)]
-pub fn sine(x: f64) -> f64 {
-    x.sin()
+fn fa(f: f64) -> f64 {
+    (2. * PI * f)/crate::SAMPLERATE
 }
 #[allow(dead_code)]
-pub fn snare(x: f64) -> f64 {
-    x.powf(2.).sin() / 1.5
+pub fn sine(freq: f64, t: f64) -> f64 {
+    (fa(freq)*t).sin()
 }
 #[allow(dead_code)]
-pub fn squeaky(x: f64) -> f64 {
-    x.tan().sin()
+pub fn snare(freq: f64, t: f64) -> f64 {
+    (fa(freq)*t).powf(2.).sin() / 1.5
 }
 #[allow(dead_code)]
-pub fn square(x: f64) -> f64 {
-    x.sin().signum()
+pub fn squeaky(freq: f64, t: f64) -> f64 {
+    (fa(freq)*t).tan().sin()
 }
 #[allow(dead_code)]
-pub fn triangle(x: f64) -> f64 {
-    x.sin().asin()
+pub fn square(freq: f64, t: f64) -> f64 {
+    (fa(freq)*t).sin().signum()
 }
 #[allow(dead_code)]
-pub fn sawtooth(x: f64) -> f64 {
-    x.tan().atan()
+pub fn triangle(freq: f64, t: f64) -> f64 {
+    (fa(freq)*t).sin().asin()
 }
 #[allow(dead_code)]
-pub fn phat_sine(x: f64) -> f64 {
-    sine(x) + sine(x / PI)
+pub fn sawtooth(freq: f64, t: f64) -> f64 {
+    (fa(freq)*t).tan().atan()
 }
 #[allow(dead_code)]
-pub fn noize(x: f64) -> f64 {
+pub fn phat_sine(freq: f64, t: f64) -> f64 {
+    sine(fa(freq), t) + sine(fa(freq) / PI, t)
+}
+#[allow(dead_code)]
+pub fn noize(freq: f64, t: f64) -> f64 {
     use rand::prelude::*;
     use byteorder::{LittleEndian, ByteOrder};
     let mut b = [0u8; 32];
-    LittleEndian::write_f64(&mut b, x);
+    LittleEndian::write_f64(&mut b, freq*t);
     let mut rng: StdRng = SeedableRng::from_seed(b);
-    rng.gen_range::<f64, f64, f64>(x-2., x)
+    rng.gen_range::<f64, f64, f64>(fa(freq)*t-2., fa(freq)*t)
 }
