6c784be54b34e840655f2e43fb59ed06f93bb577
John Dowson <dowson.john.pony@gmail.com>
2020-06-27 08:20:43 +0300

    factored out write() as a trait

--- src/main.rs
+++ src/main.rs
@@ -13,12 +13,12 @@ const SAMPLERATE: f64 = 48000.;
 const BPM: f64 = 136.;
 
 fn attack(i: f64, duration: usize) -> f64 {
-    let _duration = duration as f64;
-    let attack_length = 1000.;
+    let duration = duration as f64;
+    let attack_length = 10000.;
     let s: f64 = (i) / (attack_length);
     let r: f64;
     if i < attack_length {
-        r = lerp(0., 1., s);
+        r = lerp(0.0001, 1.1, s);
     } else {
         r = 1f64;
     }
@@ -27,11 +27,11 @@ fn attack(i: f64, duration: usize) -> f64 {
 
 fn decay(i: f64, duration: usize) -> f64 {
     let duration = duration as f64;
-    let decay_length = 2000.;
-    let s: f64 = ((duration - i)) / (decay_length);
+    let decay_length = 24000.;
+    let s: f64 = (duration - i) / (decay_length);
     let r: f64;
     if duration - i < decay_length {
-        r = lerp(-0.001, 1., s);
+        r = lerp(0.0001, 1.1, s);
     } else {
         r = 1f64;
     }
@@ -42,36 +42,28 @@ fn lerp(from: f64, to: f64, scalar: f64) -> f64 {
     from + (to - from) * scalar
 }
 
-fn freq(p: Note, waveform: fn(f64, f64) -> f64) -> Wave {
+//TODO: rework
+fn freq(p: Note, waveform: Waveform<f64>) -> Wave<f64> {
     match p {
         Note::Silence(duration) => return vec![0f64; (duration * SAMPLERATE) as usize],
         Note::Note(hz, duration) => {
             let total_samples = (duration * SAMPLERATE) as usize;
-            let volume = 0.5;
+            let volume = 0.1;
+            let mut arp = Arpeggiator::new_uninitialized();
+            //let waveform = wavedef(hz);
             return linspace::<f64>(0., duration * SAMPLERATE, total_samples)
                 .enumerate()
                 .map(|x| {
                     let (_step, t) = x;
-                    let mut sample = waveform(hz, t);
-                    sample = (attack(t, total_samples) * sample) * decay(t, total_samples);
+                    let mut sample: f64 = waveform(hz, t, &mut arp);
+                    sample =
+                        sample * (decay(t, total_samples) * 1. + attack(t, total_samples) * 1.);
                     sample * volume
                 })
-                .map(|x| x as f64)
+                //.map(|x| x as f64)
                 .collect();
         }
     }
-}
-
-fn write(p: &str, w: Wave) {
-    use byteorder::{ByteOrder, LittleEndian};
-    use std::fs::File;
-    use std::io::Write;
-    use std::mem::size_of;
-    println!("Writing to file {}", p);
-    let mut f = File::create(p).expect("Can't create specified file");
-    let mut b = vec![0u8; size_of::<Sample>() * w.len()];
-    LittleEndian::write_f64_into(&w, &mut b);
-    f.write_all(&b).expect("Can't write to specified file");
 }
 
 fn main() {
@@ -256,19 +248,18 @@ fn main() {
         Interval::Note(10., 0.5),
         Interval::Hold(4.),
     ];*/
-    wave = &[Interval::Note(0., 10.)];
+    wave = &[Interval::Note(0., 10.), Interval::Note(0., 10.)];
     let wave = wave.iter().map(|x| x.to_note(BPM, PITCH));
-    write(
-        &"out.bin",
-        wave.map(|i| {
-            freq(i, |f, t| {
-                //
-                //sine(f*2., t) + sine(f/2., t) + sine(f, t)
-                lerp(sine(f*2., t), sine(f/2., t), sine(f, fa(t)))
-            })
-        })
-        .flatten()
-        .collect(),
-    );
+    wave.map(|i| {
+        freq(i, |f, t, _| sine(f, t))
+        /*freq(i, |f, t| {
+            //lerp(sine(f*2., t), sine(f/2., t), sine(f, t))
+            //sine(f*2., t) + sine(f/2., t) + sine(f, t)
+            //lerp(sine(f*2., t), sine(f/2., t), sine(f, fa(t)))
+        })*/
+    })
+    .flatten()
+    .collect::<Wave<f64>>()
+    .write(&"out.bin");
     println!("{:?}", t1.elapsed());
 }
--- src/types.rs
+++ src/types.rs
@@ -1,9 +1,11 @@
 pub type Semitone = f64;
 pub type Hz = f64;
 pub type Second = f64;
-pub type Sample = f64;
+pub type Sample<T: num::Float> = T;
 pub type Beat = f64;
-pub type Wave = Vec<Sample>;
+pub type Wave<T: num::Float> = Vec<Sample<T>>;
+//pub type Wavedef = fn(Hz) -> Waveform;
+pub type Waveform<T: num::Float> = fn(T, T, &mut Arpeggiator<T>) -> T;
 #[derive(Debug, Clone, Copy)]
 #[allow(dead_code)]
 pub enum Interval {
@@ -49,6 +51,164 @@ impl ToSamples for usize {
     }
 }
 */
+enum Direction {
+    Forward,
+    Back,
+}
+impl Direction {
+    pub fn switch(&self) -> Direction {
+        match self {
+            Direction::Forward => Direction::Back,
+            Direction::Back => Direction::Forward,
+        }
+    }
+}
+#[allow(dead_code)]
+pub struct Arpeggiator<T: Sized> {
+    seq: Vec<T>,
+    current: usize,
+    next: usize,
+    direction: Direction,
+    step_by: T,
+    step: T,
+    initialized: bool,
+}
+#[allow(dead_code)]
+impl Arpeggiator<f64> {
+    pub fn new_uninitialized() -> Arpeggiator<f64> {
+        Arpeggiator::<f64> {
+            seq: vec![],
+            current: 0,
+            next: 0,
+            direction: Direction::Forward,
+            step_by: 0.,
+            step: 0.,
+            initialized: false,
+        }
+    }
+    pub fn init(&mut self, seq: Vec<f64>, step_by: f64) -> Result<(), &'static str> {
+        match seq.len() {
+            0..=1 => Err("Sequence must have two or more values"),
+            _ => {
+                self.seq = seq.clone();
+                self.current = 0;
+                self.next = 1;
+                self.direction = Direction::Forward;
+                self.step_by = step_by;
+                self.step = 0.;
+                self.initialized = true;
+                Ok(())
+            }
+        }
+    }
+    pub fn new(seq: Vec<f64>, step_by: f64) -> Result<Arpeggiator<f64>, &'static str> {
+        match seq.len() {
+            0..=1 => Err("Sequence must have two or more values"),
+            _ => Ok(Arpeggiator::<f64> {
+                seq: seq.clone(),
+                current: 0,
+                next: 1,
+                direction: Direction::Forward,
+                step_by: step_by,
+                step: 0.,
+                initialized: true,
+            }),
+        }
+    }
+    pub fn is_initialized(&self) -> bool {
+        self.initialized
+    }
+    // neither does this
+    /* pub fn next(&mut self) -> f64 {
+        let r: f64;
+        if self.step >= 1. {
+            r = self.seq[self.next];
+            if self.next + 1 == self.seq.len() {
+                self.current = 0;
+                self.next = 1;
+                self.step = 0.;
+            } else {
+                self.current += 1;
+                self.next += 1;
+                self.step = 0.;
+            }
+        } else {
+            r = self.seq[self.current];
+            //go on
+            self.step += self.step_by
+        }
+        return r
+    } */
+    // this monstrocity does something that might end up useful, but totally not what i intended
+    /* pub fn next(&mut self) -> f64 {
+        let r = crate::lerp(self.seq[self.current], self.seq[self.next], self.step);
+        if self.step >= 1. {
+            //next target or change direction
+            match self.direction {
+                Direction::Forward => {
+                    if self.next + 1 == self.seq.len() {
+                        self.direction = self.direction.switch();
+                        // switch current and target around
+                        self.current += 1;
+                        self.next -= 1;
+                        self.step = 0.;
+                    } else {
+                        self.current += 1;
+                        self.next += 1;
+                        self.step = 0.;
+                    }
+                }
+                Direction::Back => {
+                    if self.next == 0 {
+                        self.direction = self.direction.switch();
+                        self.current -= 1;
+                        self.next += 1;
+                        self.step = 0.;
+                    } else {
+                        self.current -= 1;
+                        self.next -= 1;
+                        self.step = 0.;
+                    }
+                }
+            }
+        } else {
+            //go on
+            self.step += self.step + self.step_by
+        }
+        return r;
+    } */
+}
+
+pub trait WriteWave<T: num::Float> {
+    fn write(&self, p: &str);
+}
+impl WriteWave<f64> for Wave<f64> {
+    fn write(&self, p: &str) {
+        use byteorder::{ByteOrder, LittleEndian};
+        use std::fs::File;
+        use std::io::Write;
+        use std::mem::size_of;
+        println!("Writing to file {}", p);
+        let mut f = File::create(p).expect("Can't create specified file");
+        let mut b = vec![0u8; size_of::<Sample<f64>>() * self.len()];
+        LittleEndian::write_f64_into(self, &mut b);
+        f.write_all(&b).expect("Can't write to specified file");
+    }
+}
+impl WriteWave<f32> for Wave<f32> {
+    fn write(&self, p: &str) {
+        use byteorder::{ByteOrder, LittleEndian};
+        use std::fs::File;
+        use std::io::Write;
+        use std::mem::size_of;
+        println!("Writing to file {}", p);
+        let mut f = File::create(p).expect("Can't create specified file");
+        let mut b = vec![0u8; size_of::<Sample<f32>>() * self.len()];
+        LittleEndian::write_f32_into(&self, &mut b);
+        f.write_all(&b).expect("Can't write to specified file");
+    }
+}
+
 fn semitone_to_hz(s: Semitone, base_pitch: Hz) -> Hz {
     let a: f64 = (2.0 as f64).powf(1. / 12.);
     return base_pitch * a.powf(s);
